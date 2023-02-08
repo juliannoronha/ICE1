9e99af28403a6e77126c0044b11da08e96776253
julian <julian@10.16.18.26>
2023-02-08 15:52:46 -0500

    first commit

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,4 +1,6 @@
 package exercise1;
+import static java.lang.Math.random;
+import java.util.Scanner;
 
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
@@ -6,7 +8,7 @@ package exercise1;
  * To be used as starting code in Exercise
  *
  * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Julian Noronha Jan 25, 2022 
  */
 public class CardTrick {
     
@@ -14,9 +16,9 @@ public class CardTrick {
         
         Card[] hand = new Card[7];
 
-        for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
-            //card.setValue(insert call to random number generator here)
+       // for (int i = 0; i < hand.length; i++) {
+            //Card card = new Card();
+            //card.setSuit(insert call to random number generator here)
             // 
             //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
@@ -32,6 +34,41 @@ public class CardTrick {
         // Then loop through the cards in the array to see if there's a match.
         
         // If the guess is successful, invoke the printInfo() method below.
+        
+        Card[] hand = new Card[7];
+        hand[0]= new Card();
+        hand[0].setValue(1);
+        hand[0].setSuit("Hearts");        
+        hand[1]= new Card();
+        hand[1].setValue(2);
+        hand[1].setSuit("Clubs");        
+        hand[2]= new Card();
+        hand[2].setValue(3);
+        hand[2].setSuit("Diomonds");
+        hand[3]= new Card();
+        hand[3].setValue(4);
+        hand[3].setSuit("Spades");
+        hand[4]= new Card();
+        hand[4].setValue(5);
+        hand[4].setSuit("Clubs");
+        hand[5]= new Card();
+        hand[5].setValue(12);
+        hand[5].setSuit("Hearts");
+        hand[6]= new Card();
+        hand[6].setValue(11);
+        hand[6].setSuit("Hearts");
+       
+        Scanner input = new Scanner(System.in);
+        int userValue =input.nextInt();
+        String userSuit= input.nextLine();
+        for(int i =0;i<hand.length;i++){
+            if(hand[i].getValue()==userValue &&
+               hand[i].getSuit().equals(userSuit)){
+                printInfo();
+            }//end of if
+        }//end of for
+        
+        
         
     }
 
@@ -58,6 +95,8 @@ public class CardTrick {
         System.out.println("-- Cooking");
         System.out.println("-- Reading/Watching TV");
         System.out.println("-- Riding my motorcycle");
+        
+        
 
         System.out.println();
         
